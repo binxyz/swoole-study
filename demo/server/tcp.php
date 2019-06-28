56218bc9501463b156985c76f6cbd0a4c0cf57f5
vagrant <vagrant@homestead>
2019-06-28 06:19:57 +0000

    tcp server

---
+++ demo/server/tcp.php
@@ -0,0 +1,20 @@
+<?php
+$serv = new swoole_server("192.168.10.10", 9501);
+$serv->set(array(
+    'worker_num' => 2,
+    'max_request' => 4
+));
+
+$serv->on('connect', function($serv, $fd) {
+    echo "客户端：{$fd}连接...\n";
+});
+
+$serv->on('receive', function($serv, $fd, $from_id, $data) {
+    $serv->send($fd, "服务发送:".$from_id.'数据'.$data."\n");
+});
+
+$serv->on('close', function($serv, $fd) {
+    echo "客户端关闭\n";
+});
+
+$serv->start();
